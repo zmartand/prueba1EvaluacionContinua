7cde728cb185872d3bc44baece7d7e9577a140e7
Zara Martín <zmartand@myuax.es>
2023-03-24 21:23:05 +0100

    Main

---
+++ src/main/java/Ejercicio2_2/Main.java
@@ -0,0 +1,9 @@
+package Ejercicio2_2;
+
+public class Main {
+
+        public static void main(String[] args) {
+            Principal p = new Principal();
+            p.Simulacion();
+        }
+}
