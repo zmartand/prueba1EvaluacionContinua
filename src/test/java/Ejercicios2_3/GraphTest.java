d14b46cc0c4919d042c1dab5f05d4a5a273e527e
Zara Martín <zmartand@myuax.es>
2023-03-25 00:16:40 +0100

    Test de clase Graph

---
+++ src/test/java/Ejercicios2_3/GraphTest.java
@@ -0,0 +1,13 @@
+package Ejercicios2_3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @org.junit.jupiter.api.Test
+    void onePathFindsAPath() {
+        System.out.println("\nTest onePathFindsAPath");
+        System.out.println("-------------------------------------- ");
+
+    }
+}
