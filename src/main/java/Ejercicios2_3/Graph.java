85f0c0735e473327242404403b0845f33836008d
Zara Martín <zmartand@myuax.es>
2023-03-25 00:11:28 +0100

    Grafo probando

--- src/main/java/Ejercicios2_3/Graph.java
+++ src/main/java/Ejercicios2_3/Graph.java
@@ -77,5 +77,16 @@ public class Graph<V> {
             return null;
         }
     }
+    public static void main(String[] args) {
+        Graph<Integer> g = new Graph<>();
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 1);
+        g.addEdge(1, 5);
+        g.addEdge(5, 6);
+        g.addEdge(6, 4);
+        System.out.println(g.toString());
+    }
 }
 
